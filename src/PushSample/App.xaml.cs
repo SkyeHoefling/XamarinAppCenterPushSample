04046249efd8f6c323d4f00d78a0081b00c2cb04
Andrew Hoefling <andrew@hoeflingsoftware.com>
2018-03-25 14:41:19 -0400

    Added app center dependencies and initialized AppCenter

--- src/PushSample/App.xaml.cs
+++ src/PushSample/App.xaml.cs
@@ -1,19 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+using Microsoft.AppCenter.Push;
 
 using Xamarin.Forms;
 
 namespace PushSample
 {
-	public partial class App : Application
+    public partial class App : Application
 	{
 		public App ()
 		{
 			InitializeComponent();
+            AppCenter.Start("android=6bdc104b-28ae-4cb8-bbf0-d7e5d2732357;ios=6b25b60d-25f4-45b1-aaed-9570197cf3cf",
+                typeof(Analytics), typeof(Crashes), typeof(Push));
 
-			MainPage = new PushSample.MainPage();
+            MainPage = new PushSample.MainPage();
 		}
 
 		protected override void OnStart ()
