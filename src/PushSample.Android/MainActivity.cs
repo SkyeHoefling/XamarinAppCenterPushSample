b4fccdd2b342d93e6ef30c97312479db77c862f7
Andrew Hoefling <andrew@hoeflingsoftware.com>
2018-03-25 15:02:23 -0400

    Added sender ID configuration for android project

--- src/PushSample.Android/MainActivity.cs
+++ src/PushSample.Android/MainActivity.cs
@@ -6,6 +6,7 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Microsoft.AppCenter.Push;
 
 namespace PushSample.Droid
 {
@@ -20,6 +21,7 @@ namespace PushSample.Droid
             base.OnCreate(bundle);
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
+            Push.SetSenderId("1058776561584");
             LoadApplication(new App());
         }
     }
